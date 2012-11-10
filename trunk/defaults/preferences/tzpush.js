085931438efd110f80e8f04d8461dde777d1539d
marknethersole@gmail.com <marknethersole@gmail.com@69a870f2-a396-3de5-0591-59945fae2a45>
2012-11-10 15:05:00 +0000

    First Commit

---
+++ trunk/defaults/preferences/tzpush.js
@@ -0,0 +1,9 @@
+pref("extensions.tzpush.host", "");
+pref("extensions.tzpush.user", "");
+pref("extensions.tzpush.password","");
+pref("extensions.tzpush.deviceId", "");
+pref("extensions.tzpush.synckey", "");
+pref("extensions.tzpush.folderID", "");
+
+// for installation
+// pref("extensions.tzpush@capeproject.org.uk", "chrome://tzpush/locale/tzpush.properties");
