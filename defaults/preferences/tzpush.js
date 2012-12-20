7c38236df2e823b6f64b11ae9c639b66f6e05f3c
marknethersole@gmail.com <marknethersole@gmail.com@69a870f2-a396-3de5-0591-59945fae2a45>
2012-12-20 13:38:35 +0000

    Provisioning Added

--- defaults/preferences/tzpush.js
+++ defaults/preferences/tzpush.js
@@ -5,8 +5,11 @@ pref("extensions.tzpush.deviceId", "");
 pref("extensions.tzpush.synckey", "");
 pref("extensions.tzpush.folderID", "");
 pref("extensions.tzpush.LastSyncTime", "");
-pref("extensions.tzpush.https","");
+pref("extensions.tzpush.https",false);
 pref("extensions.tzpush.abname","");
+pref("extensions.tzpush.polkey","0");
+pref("extensions.tzpush.prov",false);
+
 
 // for installation
 // pref("extensions.tzpush@capeproject.org.uk", "chrome://tzpush/locale/tzpush.properties");
